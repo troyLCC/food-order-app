dddc9c6ba979662c7f040642ebdeb9a354cbf058
troyLCC <timothyroy2011@gmail.com>
2021-05-22 08:32:54 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import Header from "./";
+import Header from "./components/Layout/Header"
 
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Header />
     </div>
   );
 }
